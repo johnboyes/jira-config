f6d60905d0489f34f812b2360365e0bfd3ccc479
John Boyes <154404+johnboyes@users.noreply.github.com>
2021-02-17 06:59:26 +0000

    Extract Jira class

---
+++ step_impl/jira.py
@@ -0,0 +1,27 @@
+from getgauge.python import before_suite
+from jira import JIRA
+import os
+
+
+class Jira:
+    def __init__(self, username, password):
+        self.jira = JIRA(
+            auth=(username, password),
+            options={"server": os.getenv("JIRA_BASE_URL")},
+        )
+
+    def has_group(self, group_name):
+        return len(self.jira.group_members(group_name)) >= 0
+
+    def projects(self):
+        return self.jira.projects()
+
+    def is_internal_employee_in_internal_employees_group(self):
+        internal_employees = self.jira.group_members("jira-internal-employee_users")
+        return bool(
+            [
+                emp
+                for emp in internal_employees.values()
+                if (emp["name"] == os.getenv("INTERNAL_EMPLOYEE_USERNAME"))
+            ]
+        )
--- step_impl/step_impl.py
+++ step_impl/step_impl.py
@@ -1,37 +1,15 @@
-from getgauge.python import step, before_scenario, Messages
-from jira import JIRA
+from getgauge.python import step
+from step_impl.jira import Jira
 import os
 
 
 def jira_admin():
-    return jira(os.getenv("JIRA_ADMIN_USERNAME"), os.getenv("JIRA_ADMIN_PASSWORD"))
+    return Jira(os.getenv("JIRA_ADMIN_USERNAME"), os.getenv("JIRA_ADMIN_PASSWORD"))
 
 
 def internal_employee():
-    return jira(
+    return Jira(
         os.getenv("INTERNAL_EMPLOYEE_USERNAME"), os.getenv("INTERNAL_EMPLOYEE_PASSWORD")
-    )
-
-
-def jira(username, password):
-    return JIRA(
-        auth=(username, password),
-        options={"server": os.getenv("JIRA_BASE_URL")},
-    )
-
-
-def is_group(group_name):
-    return len(jira_admin().group_members(group_name)) >= 0
-
-
-def is_internal_employee_in_internal_employees_group():
-    internal_employees = jira_admin().group_members("jira-internal-employee_users")
-    return bool(
-        [
-            emp
-            for emp in internal_employees.values()
-            if (emp["name"] == os.getenv("INTERNAL_EMPLOYEE_USERNAME"))
-        ]
     )
 
 
@@ -42,12 +20,12 @@ def an_internal_employee_can_see_all_projects():
 
 @step("An internal employee is a member of the internal employees group")
 def an_internal_employee_is_a_member_of_the_internal_employees_group():
-    assert is_internal_employee_in_internal_employees_group()
+    assert jira_admin().is_internal_employee_in_internal_employees_group()
 
 
 @step("There is a jira-internal-employee-users group")
 def there_is_a_jira_internal_employee_users_group():
-    assert is_group("jira-internal-employee_users")
+    assert jira_admin().has_group("jira-internal-employee_users")
 
 
 @step("Not implemented yet")
